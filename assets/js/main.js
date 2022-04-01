fe00125110f2484fbbc63782892758186d471a01
MartinaGangemi <martinagangemi94@gmail.com>
2022-04-01 14:52:30 +0200

    parola palindroma

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+// creo la funzione
+  function parolaPalindroma (){
+    
+    // chiedere all'utente una parola 
+   let parola = prompt("inserisci una parola")
+    // trasformo la parola in un array
+    let lettere = parola.split('');
+
+    console.log(lettere)
+
+    // inverto l'ordine dell'array
+    lettere = lettere.reverse();
+    console.log(lettere)
+
+    // creo una variabile e trasformo l'array in una stringa
+    let parolaInvertita = lettere.join('');
+    console.log(parolaInvertita);
+
+    if(parola == parolaInvertita){
+        console.log('la parola è palindroma');
+    } else {
+        console.log('la parola non è palindroma');
+    }
+
+  }
+
+  parolaPalindroma ()
+   
